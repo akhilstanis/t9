c60008e1d93b27d6cfe91ebd5831c7b820bdc336
Akhil Stanislavose <akhil@akhils-macbook-pro.local>
2011-12-25 19:17:26 +0530

    Refactored code and specs

--- lib/t9.rb
+++ lib/t9.rb
@@ -8,64 +8,25 @@ class T9
     @words = BloomFilter::Native.load '/Users/akhil/Desktop/ruby/T9/lib/words'
   end
 
-  def decode_key k
-    CHARS[k]
+  def valid_words_using keys
+    combinations(keys).select do |combination|
+      @words.include? combination
+    end
   end
 
-  def words keys
-    @buffer = nil
+  def combinations keys
+    temp = []
+    combinations = CHARS[keys.shift]
     keys.each do |key|
-      chars = CHARS[key]
-      if @buffer
-        @buffer = fill @buffer, chars
-      else
-        @buffer = map_nil chars
-      end
-    end
-    @buffer
-  end
-
-  def humanize keys
-    hash = words keys
-    extract(hash).flatten
-  end
-
-  # { 'd' => {'d' => nil, 'e' => nil , 'f' => nil }, 'e' => {'d' => nil, 'e' => nil , 'f' => nil }, 'f' => {'d' => nil, 'e' => nil , 'f' => nil } }
-  def extract h
-    h.collect do |k,v|
-      if v
-        extract(v).collect do |e|
-          k + e
+      combinations.each do |combination|
+        temp += CHARS[key].collect do |char|
+          combination + char
         end
-      else
-        k
       end
-    end.flatten
-  end
-
-  def fill h, chars
-    h.each do |k,v|
-      if v then
-        fill v,chars
-      else
-        h[k] = map_nil chars
-      end
+      combinations = temp
+      temp =[]
     end
-    h
-  end
-
-  def map_nil arr
-    hash = {}
-    arr.each do |a|
-      hash[a] = nil
-    end
-    hash
-  end
-
-  def get_valid_words keys
-    humanize(keys).select do |x|
-      @words.include? x
-    end
+    combinations
   end
 
 end
--- spec/t9_spec.rb
+++ spec/t9_spec.rb
@@ -7,26 +7,13 @@ describe T9 do
     @o = T9.new
   end
 
-  it "should decode the key" do
-    @o.decode_key(2).should ==  %w(a b c)
-    @o.decode_key(9).should ==  %w(w x y z)
+  it "should find all the combinations" do
+    @o.combinations([3]).should == ['d','e','f']
+    @o.combinations([3,6,6]).should == ["dmm", "dmn", "dmo", "dnm", "dnn", "dno", "dom", "don", "doo", "emm", "emn", "emo", "enm", "enn", "eno", "eom", "eon", "eoo", "fmm", "fmn", "fmo", "fnm", "fnn", "fno", "fom", "fon", "foo"]
   end
 
-  it "should make nil mapped hash " do
-    @o.map_nil(['a','b','c']).should == { 'a' => nil, 'b' => nil, 'c' => nil }
-  end
-
-  it "should find the possible combinations" do
-    @o.words([3]).should == {'d' => nil, 'e' => nil ,'f' => nil}
-    @o.words([3,3]).should == { 'd' => {'d' => nil, 'e' => nil , 'f' => nil }, 'e' => {'d' => nil, 'e' => nil , 'f' => nil }, 'f' => {'d' => nil, 'e' => nil , 'f' => nil } }
-  end
-
-  it "should humanize the hash" do
-    @o.humanize([3,3]).should == ['dd','de','df', 'ed','ee','ef', 'fd','fe','ff']
-  end
-
-  it "should find valid words" do
-    @o.get_valid_words([4,3,5,5,6]).should == ['hello']
+  it "should find valid words from combinations" do
+    @o.valid_words_using([4,3,5,5,6]).should == ["hello"]
   end
 
 end
