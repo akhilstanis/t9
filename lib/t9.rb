ff49eedb27d5993b4b217dc67d1b66e0a646e10f
Akhil Stanislavose <akhil@akhils-macbook-pro.local>
2011-12-25 19:28:45 +0530

    fixed loading of dictionary

--- lib/t9.rb
+++ lib/t9.rb
@@ -5,7 +5,7 @@ class T9
   CHARS = [ nil, nil, %w(a b c), %w(d e f), %w(g h i), %w(j k l), %w(m n o), %w(p q r s), %w(t u v), %w(w x y z) ]
 
   def initialize
-    @words = BloomFilter::Native.load '/Users/akhil/Desktop/ruby/T9/lib/words'
+    @words = BloomFilter::Native.load "#{File.dirname(__FILE__)}/words"
   end
 
   def valid_words_using keys
